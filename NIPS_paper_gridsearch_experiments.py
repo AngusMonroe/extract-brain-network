a90fa539b492ab7d84d0887807e1483de52b3051
Angus <1621xjx@buaa.edu.cn>
2023-02-13 21:39:06 +0800

    Update grid search with 10-fold CV

--- NIPS_paper_gridsearch_experiments.py
+++ NIPS_paper_gridsearch_experiments.py
@@ -27,9 +27,9 @@ INPUT PARAMETERS -
 Define the datasets, parcellations, and models for this run of Grid Search
 """
 
-dataset = ["abide", "adni", "ppmi", "taowu", "neurocon"]
-parcellation = ["AAL116", "harvard48", "schaefer100", "kmeans100", "ward100"]
-models = ["LR", "SVC", "kNN", "RF"]
+dataset = ["ppmi", "taowu", "neurocon"]  # ["abide", "adni", "ppmi", "taowu", "neurocon"]
+parcellation = ["schaefer100"]  # ["AAL116", "harvard48", "schaefer100", "kmeans100", "ward100"]
+models = ["LR", "NB", "SVC", "kNN", "RF"]
 
 
 """ assign ROI counts to specified parcellation schemes """
@@ -52,50 +52,59 @@ def load_data(dataset, parcellation, num_rois):
     Return [X, y] by first loading data then creating feature matrices by
     loading edge weights into X, and loading class labels into y
     """
-    subject_paths = glob.glob("datasets/" + dataset + "/*")
-    subject_names = [os.path.basename(x) for x in subject_paths]
+    group_paths = glob.glob("data/" + dataset + "_" + parcellation + "/*")
+    subject_paths = []
+    splits = {}
+    for path in group_paths:
+        if path[-6:] == '.index':
+            for split in ['train', 'val', 'test']:
+                if split in path:
+                    f = open(path, 'r')
+                    splits[split] = [[int(s) for s in l.split(',')] for l in f.readlines()]
+                    # splits[split] = np.loadtxt(path, delimiter=',', dtype=int)
+            continue
+        subject_paths += glob.glob(path + '/*')
     y = []
     X = np.zeros(num_rois * num_rois)
 
     for x in range(0, len(subject_paths)):
-        mat = loadmat(
-            subject_paths[x]
-            + "/"
-            + subject_names[x]
-            + "_"
-            + parcellation
-            + "_correlation_matrix.mat"
-        )
-        adjacency = mat["data"]
+        # mat = loadmat(subject_paths[x])
+        # adjacency = mat["data"]
+        adjacency = np.loadtxt(subject_paths[x], delimiter=' ')
 
         X = np.vstack([X, adjacency.flatten()])
 
         # generate class label list y based on subject ID
-        if "control" in subject_names[x]:
+        if "control" in subject_paths[x]:
             y.append(1)
-        elif "patient" in subject_names[x]:
+        elif "patient" in subject_paths[x]:
             y.append(2)
-        elif "mci" or "prodromal" in subject_names[x]:
+        elif "mci" or "prodromal" in subject_paths[x]:
             y.append(3)
-        elif "emci" or "swedd" in subject_names[x]:
+        elif "emci" or "swedd" in subject_paths[x]:
             y.append(4)
-        elif "SMC" in subject_names[x]:
+        elif "SMC" in subject_paths[x]:
             y.append(5)
-        elif "LMCI" in subject_names[x]:
+        elif "LMCI" in subject_paths[x]:
             y.append(6)
 
     X = np.delete(X, 0, axis=0)  # delete empty first row of zeros from X
 
-    return [X, y]
+    split_iters = []
+    if splits:
+        for fold in range(len(splits['train'])):
+            split_iters.append((splits['train'][fold], splits['val'][fold]))
+
+    return [X, y], splits, split_iters
 
 
-def cross_validation(X, y, dataset, parcellation, modelname):
+def cross_validation(X, y, dataset, parcellation, modelname, splits, split_iters):
     """
     Runs cross-validation and saves the Grid Search results
     into a .csv in the same filepath
     """
     if modelname == "LR":
-        parameters = {"penalty": ("l2", "none")}
+        parameters = {"penalty": ("l2", 'none')}
         model = LogisticRegression(max_iter=1000000)
     elif modelname == "kNN":
         parameters = {
@@ -122,18 +131,35 @@ def cross_validation(X, y, dataset, parcellation, modelname):
         parameters = {}
         model = GaussianNB()
 
-    clf = GridSearchCV(model, parameters)
+    clf = GridSearchCV(model, parameters, cv=split_iters, refit=False) if split_iters else GridSearchCV(model, parameters)
     clf.fit(X, y)
+    best_params = clf.best_params_
 
     df = pd.DataFrame.from_dict(clf.cv_results_)
-    filepath = Path(dataset + "_" + parcellation + "_" + modelname + ".csv")
+    filepath = Path('result/' + dataset + "_" + parcellation + "_" + modelname + ".csv")
     df.to_csv(filepath)
 
     print("------------------------------")
     print("dataset:", dataset)
+    print('subject number:', len(y))
     print("parcellation:", parcellation)
     print("model:", modelname)
     print("DONE")
+    if splits:
+        accs = []
+        for i, test_idx in enumerate(splits['test']):
+            train_idx = splits['train'][i]
+            train_X = X[train_idx]
+            train_y = np.array(y)[train_idx]
+            fold_clf = model.set_params(**best_params)
+            fold_clf.fit(train_X, train_y)
+            predicted = fold_clf.predict(X[test_idx])
+            test_acc = sum(predicted == np.array(y)[test_idx]) / len(np.array(y)[test_idx])
+            accs.append(test_acc)
+        acc_mean = np.mean(np.array(accs))
+        acc_std = np.std(np.array(accs))
+        print('test mean acc: {}'.format(acc_mean))
+        print('test acc std: {}'.format(acc_std))
     print("------------------------------")
 
 
@@ -141,5 +167,5 @@ def cross_validation(X, y, dataset, parcellation, modelname):
 for i in range(0, len(dataset)):
     for j in range(0, len(parcellation)):
         for k in range(0, len(models)):
-            data = load_data(dataset[i], parcellation[j], num_roi[j])
-            cross_validation(data[0], data[1], dataset[i], parcellation[j], models[k])
+            data, splits, split_iters = load_data(dataset[i], parcellation[j], num_roi[j])
+            cross_validation(data[0], data[1], dataset[i], parcellation[j], models[k], splits, split_iters)
